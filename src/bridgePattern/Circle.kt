dc807dfb2c755567244ae0e7b7b65d9fb28947cc
SAR-Khadem <sar.khadem2000@gmail.com>
2020-04-21 15:45:14 +0430

    add bridge pattern

--- src/bridgePattern/Circle.kt
+++ src/bridgePattern/Circle.kt
@@ -2,7 +2,7 @@ package bridgePattern
 
 class Circle(drawAPI: DrawAPI, private var radius: Int, private var x: Int, private var y: Int) : Shape(drawAPI) {
 
-`
+
     override fun draw() {
         this.drawAPI.drawCircle(radius, x, y )
 
